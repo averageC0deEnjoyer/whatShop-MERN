345bf983214d1b1616c21eaf0fae940ab3b831e5
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-12 00:14:14 +0700

    connect to mongo DB

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log('helo!');
+  });
+});
 
 const app = express();
 
@@ -22,8 +29,4 @@ app.get('/api/products/:id', (req, res) => {
   const { id: productId } = req.params;
   const selectedProduct = products.find((product) => product._id === productId);
   res.json(selectedProduct);
-});
-
-app.listen(port, () => {
-  console.log('helo!');
 });
