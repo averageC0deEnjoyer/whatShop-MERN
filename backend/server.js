f217cef00b0d3cd456800fadaca19d340c9dfa93
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-11 22:58:40 +0700

    initialize route backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import products from './data/products.js';
 const port = 5000;
 const app = express();
 
 app.get('/', (req, res) => {
   return res.json({ msg: 'hola!' });
+});
+
+app.get('/api/products', (req, res) => {
+  return res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const { id: productId } = req.params;
+  const selectedProduct = products.find((product) => product._id === productId);
+  res.json(selectedProduct);
 });
 
 app.listen(port, () => {
