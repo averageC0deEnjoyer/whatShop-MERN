9ab4ef45ba7be3c95c3dd30eaed7c03d7abb3de7
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-11 15:36:29 +0700

    add HomeScreen, Product component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screen/HomeScreen';
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to whatShop!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/react.svg';
 
 const Header = () => {
   return (
     <header>
       <Navbar expand="md" bg="dark" variant="dark" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">whatShop</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="logo" />
+            whatShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
