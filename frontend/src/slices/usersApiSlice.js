03fce7fba416fd185d55a26998c365b09f42c148
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-15 22:09:48 +0700

    initialize authSlice and usersApiSlice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,12 @@
+import { USER_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url,
+      }),
+    }),
+  }),
+});
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
+import authSliceReducer from './slices/authSlice';
 //boilerplate
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
