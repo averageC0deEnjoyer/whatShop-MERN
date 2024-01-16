c1e7ef85a6e62f4b621b1c8b6007781423b0ec36
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-16 12:25:48 +0700

    create userAPISlice for login

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,12 +1,16 @@
-import { USER_URL } from '../constants';
+import { USERS_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url,
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
       }),
     }),
   }),
 });
+
+export const { useLoginMutation } = usersApiSlice;
