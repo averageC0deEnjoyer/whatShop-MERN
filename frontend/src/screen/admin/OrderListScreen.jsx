8e72f279bca01c85ef688b5fe0d35a739b8dc31c
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-19 15:04:50 +0700

    add OrderListScreen for admin

---
+++ frontend/src/screen/admin/OrderListScreen.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useGetAllOrdersQuery } from '../../slices/ordersApiSlice';
+import Loader from '../../components/Loader';
+import Message from '../../components/Message';
+import { Table, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const OrderListScreen = () => {
+  const { data: orders, isLoading, error } = useGetAllOrdersQuery();
+
+  return (
+    <>
+      <h1>Orders</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped hover responsive className="table-md">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID STATUS</th>
+              <th>DELIVER STATUS</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user.name}</td>
+                <td>{order.createdAt}</td>
+                <td>${order.totalPrice}</td>
+                <td>{order.isPaid ? order.paidAt : 'X'}</td>
+                <td>{order.isDelivered ? order.deliveredAt : 'X'}</td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      Detail
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default OrderListScreen;
