e2438da186d049b261cbaa40ee41b933cb3e88a2
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-20 13:27:56 +0700

    refactor

--- frontend/src/screen/admin/EditProductScreen.jsx
+++ frontend/src/screen/admin/EditProductScreen.jsx
@@ -6,7 +6,7 @@ import {
 import { Button, Form } from 'react-bootstrap';
 import FormContainer from '../../components/FormContainer';
 import { toast } from 'react-toastify';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import Loader from '../../components/Loader';
 import Message from '../../components/Message';
 
@@ -61,42 +61,47 @@ const EditProductScreen = () => {
     }
   };
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant="danger">{error}</Message>
-  ) : (
-    <FormContainer>
-      <h2>Edit Product</h2>
-      <Form onSubmit={() => {}}>
-        <Form.Group controlId="name" className="my-2">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="brand" className="my-2">
-          <Form.Label>Brand</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter brand"
-            value={brand}
-            onChange={(e) => setBrand(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="category" className="my-2">
-          <Form.Label>Category</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        {/*<Form.Group controlId="image" className="my-2">
+  return (
+    <>
+      <Link to={`/admin/productlist`} className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h2>Edit Product</h2>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={() => {}}>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="brand" className="my-2">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="enter brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="category" className="my-2">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="enter category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            {/*<Form.Group controlId="image" className="my-2">
               <Form.Label>Image</Form.Label>
               <Form.Control
               type="text"
@@ -105,36 +110,38 @@ const EditProductScreen = () => {
               onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
             </Form.Group> */}
-        <Form.Group controlId="description" className="my-2">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="price" className="my-2">
-          <Form.Label>Price</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="countInStock" className="my-2">
-          <Form.Label>Count In Stock</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter countInStock"
-            value={countInStock}
-            onChange={(e) => setCountInStock(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Button onClick={updateHandler}>Submit</Button>
-      </Form>
-    </FormContainer>
+            <Form.Group controlId="description" className="my-2">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="enter description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="price" className="my-2">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="enter price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="countInStock" className="my-2">
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="enter countInStock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button onClick={updateHandler}>Submit</Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
   );
 };
 
